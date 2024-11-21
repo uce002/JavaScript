57cb4ca22508d6d12925f469714ee24e4d1bd394
Uzair Yousaf <uzairyousaf1@icloud.com>
2024-11-21 19:09:41 +0000

    learned basic variables and how to use them as string literals to output them inside of text

--- index.js
+++ index.js
@@ -6,7 +6,37 @@ console.log('i like pizza');
 window.alert('This is an alert!');
 */
 
-document.getElementById("myH1").textContent = "1. Basics";
+document.getElementById(`myH1`).textContent = `1. Basics`;
 document.getElementById("myP").textContent = "I like pizza";
 
-// 2
+// 2 VARIABLES
+/* variable = a container that stores a value. It behaves as if it were the value it contains
+
+1. declaration =    let x;
+2. assignment  =    x = 100; */
+
+let x = 100; // if you know what the value is you can declare & assign it in 1 line
+console.log(x);
+
+let price = 10.99;
+let firstName = "Uce";
+let email = "test@gmail.com";
+// number, string, email, boolean
+
+//console.log(`You are ${age} years old`); //use backticks `` for embedding variables (template literals). otherwise use ""
+console.log(`The price is £${price}`); //£ goes before $ otherwise its included in the literal
+
+console.log(`Your name is ${firstName}`);
+console.log(typeof firstName);
+
+console.log(`Your email is ${email}`);
+
+
+let fullName = "Uce McJuce";
+let age = 22;
+let isStudent = false;
+
+document.getElementById("variable-h1").textContent = "2. Variables";
+document.getElementById("variable-p1").textContent = `Your name is ${fullName}`;
+document.getElementById("variable-p2").textContent = `Your age is ${age}`;
+document.getElementById("variable-p3").textContent = `${fullName} is a student: ${isStudent}`;
