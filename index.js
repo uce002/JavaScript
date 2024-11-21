fd7814bedb43131a41cce21eb6c3774808e0bbb7
Uzair Yousaf <uzairyousaf1@icloud.com>
2024-11-21 17:52:42 +0000

    learned the very basics of javascript like how to display text by getting an element and via the console log.

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1 BASICS
+
+/*console.log('Hello');
+console.log('i like pizza');
+
+window.alert('This is an alert!');
+*/
+
+document.getElementById("myH1").textContent = "1. Basics";
+document.getElementById("myP").textContent = "I like pizza";
+
+// 2
