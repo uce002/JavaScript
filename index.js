d3343c0de2f9ac809251a3407727338aa27ee0cf
Uzair Yousaf <uzairyousaf1@icloud.com>
2024-11-22 17:41:32 +0000

    used css, html and js to make a counter program. 3 functions for the 3 buttons to increase, decrease and reset the number on screen

--- index.js
+++ index.js
@@ -128,3 +128,26 @@ document.getElementById("radiusSubmit").onclick = function() {
     circumference = 2 * PI * radius
     document.getElementById("circumf-p").textContent = circumference + " cm";
 }
+
+
+// 7 COUNTER PROGRAM
+document.getElementById("counter-h1").textContent = "7. Counter Program";
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+}
