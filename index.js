3d2918427178b4037a35b5f5487b714f15926aa7
Uzair Yousaf <uzairyousaf1@icloud.com>
2024-12-05 15:00:45 +0000

    learned the static keyword. not sure for its use but it makes properties/methods belong to the class itself so you can call it without declaring a const and then console that

--- index.js
+++ index.js
@@ -1185,3 +1185,46 @@ product3.displayProduct();
 
 const classTotal = product3.calculateTotal(salesTax);
 console.log(`Total price (with tax): £${classTotal.toFixed(2)}`);
+
+
+// 41 STATIC
+document.getElementById("static-h1").textContent = "41. Static";
+
+// static = keyword that defines properties or methods that belong to a class itself rather than the objects created from that class (class owns anything static, not the objects)
+class MathUtil {
+    static PI = 3.14159;
+
+    static getDiameter(radius) {
+        return radius * 2;
+    }
+    static getCircumference(radius) {
+        return 2 * this.PI * radius;
+    }
+}
+console.log(MathUtil.PI); // don't need to declare const MathUtil1 = new MathUtil etc. you just call it because the class owns it so you can call it directly
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
+
+class User {
+    static userCount = 0;
+    
+    constructor(username) {
+        this.username = username;
+        User.userCount++;
+    }
+    static getUserCount() {
+        console.log(`There are ${User.userCount} users online`);
+    }
+    sayHello() {
+        console.log(`Hello my username is ${this.username}`);
+    }
+}
+
+const user1 = new User("Spongebob");
+const user2 = new User("Patrick");
+const user3 = new User("Gary");
+
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+User.getUserCount();
